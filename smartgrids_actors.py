ca73bfd925d00b47f5e24eb6cf3b874ab8ee5b27
EHOUNOU Joseph-Wilfried <jwehounou@cesi.fr>
2020-09-18 18:57:13 +0200

    classe agent compile et debugge OK, sa fonction unitaire pas encore ecrite NOK.

--- smartgrids_actors.py
+++ smartgrids_actors.py
@@ -4,18 +4,36 @@ Created on Fri Sep 18 12:51:47 2020
 
 @author: jwehounou
 """
-cpt_agents = 0
+import time
+import numpy as np
+
+import fonctions_auxiliaires as fct_aux
+
+N_INSTANCE = 10
+
+MODES = ("CONS","PROD","DIS")
+
+STATE1_STRATS = ("CONS+", "CONS-")                                             # strategies possibles pour l'etat 1 de a_i
+STATE2_STRATS = ("CONS-", "DIS")                                               # strategies possibles pour l'etat 2 de a_i
+STATE3_STRATS = ("PROD", "DIS")                                                # strategies possibles pour l'etat 3 de a_i
+
 class Agent:
     
+    cpt_agent =  0
+    
     def __init__(self, Pi, Ci, Si, Si_max, gamma_i):
-        self.name = ().join(["a",str(cpt_agents)])
+        self.name = ("").join(["a",str(self.cpt_agent)])
         self.Pi = Pi
         self.Ci = Ci
         self.Si = Si
         self.Si_max = Si_max
         self.gamma_i = gamma_i
-        cpt_agents += 1
+        Agent.cpt_agent += 1
+       
         
+    #--------------------------------------------------------------------------
+    #           definition of caracteristics of an agent
+    #--------------------------------------------------------------------------
     def get_Pi(self):
         """
         return the value of quantity of production
@@ -94,4 +112,86 @@ class Agent:
         """
         self.gamma_i = new_gamma_i 
                         
+    #--------------------------------------------------------------------------
+    #           definition of functions of an agent
+    #--------------------------------------------------------------------------
+    def identify_state(self):
+        """
+        determine the state of an agent following its characteristics
+
+        Returns
+        -------
+        state_ai = {"state1", "state2", "state3", None}
+
+        """
+        if self.Pi + self.Si <= self.Ci:
+            return "state1"
+        elif self.Pi + self.Si > self.Ci and self.Pi < self.Ci:
+            return "state2"
+        elif self.Pi > self.Ci:
+            return "state3"
+
+        return None
     
+    def select_mode(self, state_ai):
+        """
+        choose a mode for an agent under its state and its characteristics
+
+        Returns
+        -------
+        mode_i, prod_i, cons_i
+
+        """
+        res = (None, np.inf, np.inf)
+        rd_num =  np.random.choice([0,1])
+        if state_ai == None:
+            print("Conditions non respectees pour state1,2,3")
+        elif state_ai == "state1":
+            mode_i = STATE1_STRATS[rd_num]
+            prod_i = 0
+            cons_i = (1-rd_num)*( self.Ci - (self.Pi - self.Si) ) \
+                        + rd_num*(self.Ci - self.Pi) 
+            self.Si = (1-rd_num)*0 + rd_num * self.Si
+            res = (mode_i, prod_i, cons_i)
+        elif state_ai == "state2":
+            mode_i = STATE2_STRATS[rd_num]
+            prod_i = 0
+            cons_i = (1-rd_num)*(self.Ci - self.Pi) + rd_num*0
+            self.Si = (1-rd_num)*0 + rd_num*(self.Si - (self.Ci - self.Pi))
+            res = (mode_i, prod_i, cons_i)
+        elif state_ai == "state3":
+            mode_i = STATE3_STRATS[rd_num]
+            cons_i = 0
+            self.Si = (1-rd_num)*self.Si \
+                        + rd_num*(max(self.Si_max, 
+                                       self.Si + (self.Pi - self.Ci))) 
+            Ri = self.Si_max - self.Si
+            prod_i = (1-rd_num)*(self.Pi - self.Ci) \
+                        + rd_num*fct_aux.fct_positive(sum([self.Pi]), 
+                                                      sum([self.Ci, Ri]))
+            res = (mode_i, prod_i, cons_i)
+        return res
+    
+#------------------------------------------------------------------------------
+#           unit test of functions
+#------------------------------------------------------------------------------
+def test_classe_agent():
+    Pis = np.random.randint(1, 30, N_INSTANCE) + np.random.randn(1, N_INSTANCE)
+    Cis = np.random.randint(1, 30, N_INSTANCE) + np.random.randn(1, N_INSTANCE)
+    Sis = np.random.randint(1, 15, N_INSTANCE) + np.random.randn(1, N_INSTANCE) 
+    Si_maxs = np.random.randint(1, 30, N_INSTANCE) + np.random.randn(1, N_INSTANCE) 
+    gamma_is = np.random.randint(1, 3, N_INSTANCE) + np.random.randn(1, N_INSTANCE)
+    
+    # utiliser itertools pour des tuples de pi, ci, si, ...
+    # creer des instances d'agents
+    # faire les fonctions. 
+        # pour chaque fonction, calculer le rapport de ri = OK/(NOK+OK)
+    # les afflicher ces indicateurs ri
+    
+#------------------------------------------------------------------------------
+#           execution
+#------------------------------------------------------------------------------
+if __name__ == "__main__":
+    ti = time.time()
+    test_classe_agent()
+    print("classe agent runtime = {}".format(time.time() - ti))
