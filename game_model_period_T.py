f21bba9e40718ec0d11cc7eee6b8efc5067fcce4
EHOUNOU Joseph-Wilfried <jwehounou@cesi.fr>
2020-09-27 10:21:14 +0200

    modification du type de retour de la fonction determine_new_pricing_sg et modification des parametres de la fonction extract_values_to_array dans la fonction game_model_SG_T.

--- game_model_period_T.py
+++ game_model_period_T.py
@@ -396,19 +396,12 @@ def game_model_SG_T(T, pi_hp_plus, pi_hp_minus, pi_0_plus, pi_0_minus, case):
         BENs.append(bens), CSTs.append(csts)
         
         #compute the new prices pi_0_plus and pi_0_minus from a pricing model in the document
-        # prod_is = extract_values_to_array(arr_pls_M_T, t, attribut_position=5)
-        # cons_is = extract_values_to_array(arr_pls_M_T, t, attribut_position=6)
-        # pi_0_plus, pi_0_minus = determine_new_pricing_sg(
-        #                             prod_is, cons_is,
-        #                             pi_hp_plus, pi_hp_minus, t)
-        ##--- modification fcts extract_values_to_array et determine_new_pricing_sg --> debut
         prod_is_0_t = extract_values_to_array(arr_pls_M_T, range(0,t+1), attribut_position=5)
         cons_is_0_t = extract_values_to_array(arr_pls_M_T, range(0,t+1), attribut_position=6)
-        # TODO replace pi_0_plus by new_pi_0_plus
-        pi_0_plus, pi_0_minus = determine_new_pricing_sg(
-                                    prod_is_0_t, cons_is_0_t,
-                                    pi_hp_plus, pi_hp_minus, t)
-        ##--- modification fcts extract_values_to_array et determine_new_pricing_sg --> fin
+        new_pi_0_plus, new_pi_0_minus = determine_new_pricing_sg(
+                                            prod_is_0_t, cons_is_0_t,
+                                            pi_hp_plus, pi_hp_minus, t)
+        pi_0_plus, pi_0_minus = new_pi_0_plus, new_pi_0_minus
         # update CONS_i for a each player
         arr_pls = update_player(arr_pls, [(2,cons_is), (2,prod_is)])
         
