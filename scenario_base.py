0b92bd01e09978a142a693f7fb82b2c04a19761f
EHOUNOU Joseph-Wilfried <jwehounou@cesi.fr>
2021-01-11 14:26:27 +0100

    ajout des fonctions associes a la brute force.

--- scenario_base.py
+++ scenario_base.py
@@ -22,6 +22,7 @@ import smartgrids_players as players
 import fonctions_auxiliaires as fct_aux
 import game_model_period_T as gmpT
 import deterministic_game_model as detGameModel
+import force_brute_game_model as bfGameModel
 import lri_game_model as lriGameModel
 import visu_bkh_scenBase as vizScenBase
 
@@ -300,6 +301,218 @@ def scenario_base_det(arr_pl_M_T_vars_init, pi_hp_plus=0.0002,
     print("determinist game: {}, pi_hp_plus={}, pi_hp_minus ={} ---> end \n"\
           .format(scenario, pi_hp_plus, pi_hp_minus))
         
+    return arr_pl_M_T_vars
+
+
+#______________________________________________________________________________
+#
+#                   ALGO  BRUTE FORCE 
+#______________________________________________________________________________
+
+def scenario_base_bf(arr_pl_M_T_vars_init, pi_hp_plus=0.0002, 
+                        pi_hp_minus=0.33,
+                        m_players=4, 
+                        num_periods=2, 
+                        k_steps=5,
+                        prob_Ci=0.3, 
+                        learning_rate=0.01,
+                        probs_modes_states=[0.5, 0.5, 0.5],
+                        scenario="scenario_base",
+                        algo_name="BEST-BRUTE-FORCE",
+                        path_to_save="tests", dbg=False):
+    
+    # pi_sg_{plus, minus}, pi_0_{plus, minus}
+    pi_sg_plus_t, pi_sg_minus_t = 5, 4
+    pi_0_plus_t, pi_0_minus_t = 3, 1
+    
+    # constances 
+    t = 0
+    cpt_error_gamma = 0; cpt_balanced = 0;
+    dico_state_mode_i = {}; dico_balanced_pl_i = {}
+    
+    # add new columns to arr_pl_M_T
+    fct_aux.INDEX_ATTRS["Si_minus"] = 16
+    fct_aux.INDEX_ATTRS["Si_plus"] = 17
+    nb_vars_2_add = 2
+    arr_pl_M_T_vars = np.zeros((arr_pl_M_T_vars_init.shape[0],
+                                arr_pl_M_T_vars_init.shape[1],
+                                arr_pl_M_T_vars_init.shape[2]+nb_vars_2_add), 
+                               dtype=object)
+    arr_pl_M_T_vars[:,:,:-nb_vars_2_add] = arr_pl_M_T_vars_init
+    arr_pl_M_T_vars[:,:, fct_aux.INDEX_ATTRS["Si_minus"]] = np.nan
+    arr_pl_M_T_vars[:,:, fct_aux.INDEX_ATTRS["Si_plus"]] = np.nan
+    
+    # _____   balanced players at t   _______
+    arr_pl_M_T_vars, possibles_modes = bfGameModel.reupdate_state_players(
+                                        arr_pl_M_T_vars.copy(), 0, 0)
+    
+    mode_profiles = it.product(*possibles_modes)
+        
+    dico_mode_profs = dict()
+    cpt_xxx = 0
+    
+    for mode_profile in mode_profiles:
+        dico_balanced_pl_i_mode_prof, cpt_balanced_mode_prof = dict(), 0
+        dico_state_mode_i_mode_prof = dict()
+        
+        arr_pl_M_T_vars_mode_prof, \
+        dico_balanced_pl_i_mode_prof, \
+        dico_state_mode_i_mode_prof, \
+        cpt_balanced_mode_prof \
+            = bfGameModel.balanced_player_game_4_mode_profil(
+                arr_pl_M_T_vars.copy(),
+                mode_profile, t,
+                pi_sg_plus_t, pi_sg_minus_t, 
+                pi_hp_plus, pi_hp_minus,
+                dico_balanced_pl_i_mode_prof, 
+                dico_state_mode_i_mode_prof,
+                cpt_balanced_mode_prof,
+                m_players, num_periods
+                )
+        
+        # compute In_sg, Out_sg
+        In_sg, Out_sg = fct_aux.compute_prod_cons_SG(
+                            arr_pl_M_T_vars_mode_prof, 
+                            t)
+        diff_In_Out_sg = In_sg-Out_sg
+        if diff_In_Out_sg in dico_mode_profs:
+            dico_mode_profs[diff_In_Out_sg].append(mode_profile)
+        else:
+            dico_mode_profs[diff_In_Out_sg] = [mode_profile]
+         
+        cpt_xxx += 1
+    
+    # max_min_moy_bf
+    best_key_In_Out_sg = None
+    if algo_name == fct_aux.ALGO_NAMES_BF[0]:              # BEST-BRUTE-FORCE
+        best_key_In_Out_sg = min(dico_mode_profs.keys())
+    elif algo_name == fct_aux.ALGO_NAMES_BF[1]:            # BAD-BRUTE-FORCE
+        best_key_In_Out_sg = max(dico_mode_profs.keys())
+    elif algo_name == fct_aux.ALGO_NAMES_BF[2]:            # MIDDLE-BRUTE-FORCE
+        mean_key_In_Out_sg  = np.mean(list(dico_mode_profs.keys()))
+        if mean_key_In_Out_sg in dico_mode_profs.keys():
+            best_key_In_Out_sg = mean_key_In_Out_sg
+        else:
+            sorted_keys = sorted(dico_mode_profs.keys())
+            boolean = True; i_key = 1
+            while boolean:
+                if sorted_keys[i_key] <= mean_key_In_Out_sg:
+                    i_key += 1
+                else:
+                    boolean = False; i_key -= 1
+            best_key_In_Out_sg = sorted_keys[i_key]
+                
+    # find the best, bad, middle key in dico_mode_profs and 
+    # the best, bad, middle mode_profile
+    best_mode_profiles = dico_mode_profs[best_key_In_Out_sg]
+    best_mode_profile = None
+    if len(best_mode_profiles) == 1:
+        best_mode_profile = best_mode_profiles[0]
+    else:
+        rd = np.random.randint(0, len(best_mode_profiles))
+        best_mode_profile = best_mode_profiles[rd]
+    
+    print("cpt_xxx={}, best_key_In_Out_sg={}, best_mode_profile={}".format(
+            cpt_xxx, best_key_In_Out_sg, best_mode_profile))
+    
+    arr_pl_M_T_vars_mode_prof_best, \
+    dico_balanced_pl_i, dico_state_mode_i, \
+    cpt_balanced \
+        = bfGameModel.balanced_player_game_4_mode_profil(
+            arr_pl_M_T_vars.copy(),
+            best_mode_profile, t,
+            pi_sg_plus_t, pi_sg_minus_t, 
+            pi_hp_plus, pi_hp_minus,
+            dico_balanced_pl_i, dico_state_mode_i, 
+            cpt_balanced,
+            m_players, num_periods
+            )
+    
+    dico_stats_res = dict()
+    dico_stats_res[t] = (round(cpt_balanced/m_players, fct_aux.N_DECIMALS),
+                     round(cpt_error_gamma/m_players, fct_aux.N_DECIMALS), 
+                     dico_state_mode_i)
+    dico_stats_res[t] = {"balanced": dico_balanced_pl_i, 
+                         "gamma_i": dico_state_mode_i}
+    
+    # compute the new prices pi_sg_plus_t+1, pi_sg_minus_t+1 
+    # from a pricing model in the document
+    pi_sg_plus_t_new, pi_sg_minus_t_new \
+        = fct_aux.determine_new_pricing_sg(
+                arr_pl_M_T_vars_mode_prof_best.copy(), 
+                pi_hp_plus, 
+                pi_hp_minus, 
+                t, dbg=dbg)
+    print("t={}, pi_sg_plus_t_new={}, pi_sg_minus_t_new={}".format(
+        t, pi_sg_plus_t_new, pi_sg_minus_t_new))  if dbg else None
+    print("t={}, pi_sg_plus_t_new={}, pi_sg_minus_t_new={}".format(
+        t, pi_sg_plus_t_new, pi_sg_minus_t_new))                
+    pi_sg_plus_t = pi_sg_plus_t if pi_sg_plus_t_new is np.nan \
+                                else pi_sg_plus_t_new
+    pi_sg_minus_t = pi_sg_minus_t if pi_sg_minus_t_new is np.nan \
+                                else pi_sg_minus_t_new
+    pi_0_plus_t_new = round(pi_sg_minus_t*pi_hp_plus/pi_hp_minus, 
+                        fct_aux.N_DECIMALS)
+    pi_0_minus_t_new = pi_sg_minus_t
+    
+    ## compute prices inside smart grids
+    # compute In_sg, Out_sg
+    In_sg, Out_sg = fct_aux.compute_prod_cons_SG(
+                    arr_pl_M_T_vars_mode_prof_best, t)
+    diff = np.abs(best_key_In_Out_sg-(In_sg-Out_sg))
+    print("best_key==In_sg-Out_sg --> OK (diff={}) \n".format(diff)) \
+        if diff < 0.1 \
+        else print("best_key==In_sg-Out_sg --> NOK (diff={}) \n".format(diff))
+    # compute prices of an energy unit price for cost and benefit players
+    b0_t, c0_t = fct_aux.compute_energy_unit_price(
+                    pi_0_plus_t, pi_0_minus_t, 
+                    pi_hp_plus, pi_hp_minus,
+                    In_sg, Out_sg)
+
+    # compute ben, cst of shape (M_PLAYERS,) 
+    # compute cost (csts) and benefit (bens) players by energy exchanged.
+    gamma_is_t = arr_pl_M_T_vars_mode_prof_best[
+                    :, t, fct_aux.INDEX_ATTRS["gamma_i"]]
+    bens_t, csts_t = fct_aux.compute_utility_players(
+                        arr_pl_M_T_vars_mode_prof_best, 
+                        gamma_is_t, 
+                        t, 
+                        b0_t, 
+                        c0_t)
+    
+    arr_pl_M_T_vars[:,t,:] = arr_pl_M_T_vars_mode_prof_best[:,t,:].copy()
+    
+    # B_is, C_is of shape (M_PLAYERS, )
+    prod_i_T = arr_pl_M_T_vars[:,t, fct_aux.INDEX_ATTRS["prod_i"]]
+    cons_i_T = arr_pl_M_T_vars[:,t, fct_aux.INDEX_ATTRS["cons_i"]]
+    B_is = b0_t * prod_i_T
+    C_is = c0_t * cons_i_T
+    
+    # BB_is, CC_is, RU_is of shape (M_PLAYERS, )
+    CONS_is = np.sum(arr_pl_M_T_vars[:,:, fct_aux.INDEX_ATTRS["cons_i"]], axis=1)
+    PROD_is = np.sum(arr_pl_M_T_vars[:,:, fct_aux.INDEX_ATTRS["prod_i"]], axis=1)
+    BB_is = pi_sg_plus_t * PROD_is #np.sum(PROD_is)
+    for num_pl, bb_i in enumerate(BB_is):
+        if bb_i != 0:
+            print("player {}, BB_i={}".format(num_pl, bb_i))
+    CC_is = pi_sg_minus_t * CONS_is #np.sum(CONS_is)
+    RU_is = BB_is - CC_is
+    
+    
+    # save variables to scenario_base directory
+    msg = "pi_hp_plus_"+str(pi_hp_plus)+"_pi_hp_minus_"+str(pi_hp_minus)
+    path_to_save = os.path.join(path_to_save, scenario, str(prob_Ci), 
+                                msg,algo_name)
+    fct_aux.save_variables(path_to_save, arr_pl_M_T_vars, 
+                   b0_t, c0_t, B_is, C_is, 
+                   bens_t, csts_t, 
+                   BB_is, CC_is, RU_is, 
+                   pi_sg_minus_t_new, pi_sg_plus_t_new, 
+                   pi_0_minus_t_new, pi_0_plus_t_new,
+                   [pi_hp_plus], [pi_hp_minus], 
+                   dico_stats_res={}, 
+                   algo=algo_name)
+    
     return arr_pl_M_T_vars
 #______________________________________________________________________________
 #
@@ -1510,6 +1723,48 @@ def execution_algos(algos, arr_pl_M_T_vars_init,
                                   utility_function_version=utility_function_version,
                                   path_to_save=path_to_save, 
                                   dbg=False)
+        elif algo == fct_aux.ALGO_NAMES_BF[0]:              # BEST-BRUTE-FORCE
+            arr_pl_M_T_vars = scenario_base_bf(arr_pl_M_T_vars_init, 
+                                  pi_hp_plus=pi_hp_plus, 
+                                  pi_hp_minus=pi_hp_minus,
+                                  m_players=m_players, 
+                                  num_periods=num_periods, 
+                                  k_steps=k_steps,
+                                  prob_Ci=prob_Ci, 
+                                  learning_rate=learning_rate,
+                                  probs_modes_states=probs_modes_states,
+                                  scenario=scenario,
+                                  algo_name=algo,
+                                  path_to_save=path_to_save, 
+                                  dbg=False)
+        elif algo == fct_aux.ALGO_NAMES_BF[1]:            # BAD-BRUTE-FORCE
+            arr_pl_M_T_vars = scenario_base_bf(arr_pl_M_T_vars_init, 
+                                  pi_hp_plus=pi_hp_plus, 
+                                  pi_hp_minus=pi_hp_minus,
+                                  m_players=m_players, 
+                                  num_periods=num_periods, 
+                                  k_steps=k_steps,
+                                  prob_Ci=prob_Ci, 
+                                  learning_rate=learning_rate,
+                                  probs_modes_states=probs_modes_states,
+                                  scenario=scenario,
+                                  algo_name=algo,
+                                  path_to_save=path_to_save, 
+                                  dbg=False)
+        elif algo == fct_aux.ALGO_NAMES_BF[2]:            # MIDDLE-BRUTE-FORCE
+            arr_pl_M_T_vars = scenario_base_bf(arr_pl_M_T_vars_init, 
+                                  pi_hp_plus=pi_hp_plus, 
+                                  pi_hp_minus=pi_hp_minus,
+                                  m_players=m_players, 
+                                  num_periods=num_periods, 
+                                  k_steps=k_steps,
+                                  prob_Ci=prob_Ci, 
+                                  learning_rate=learning_rate,
+                                  probs_modes_states=probs_modes_states,
+                                  scenario=scenario,
+                                  algo_name=algo,
+                                  path_to_save=path_to_save, 
+                                  dbg=False)
         elif algo == "LRI1":
             arr_pl_M_T_vars = scenario_base_LRI(arr_pl_M_T_vars_init,
                              pi_hp_plus=pi_hp_plus, 
@@ -1559,7 +1814,7 @@ if __name__ == "__main__":
     scenarios=["scenario1"]
     prob_Cis=0.3
     date_hhmm="1611_1041"
-    algos= ["DETERMINIST","LRI1","LRI2"]
+    algos= ["DETERMINIST","LRI1","LRI2"] + fct_aux.ALGO_NAMES_BF
     scenario="SCENARIO_BASE"
     learning_rate = 0.1#0.01 # list(np.arange(0.05, 0.15, step=0.05))
     pi_hp_plus = 0.2*pow(10,-3)
@@ -1581,8 +1836,11 @@ if __name__ == "__main__":
         prices_new, algos_new, learning_rates_new \
             = vizScenBase.get_tuple_paths_of_arrays(name_simu=name_simu)
     df_arr_M_T_Ks, df_ben_cst_M_T_K, df_b0_c0_pisg_pi0_T_K, df_B_C_BB_CC_RU_M \
-        = vizScenBase.get_array_turn_df_for_t_scenBase(tuple_paths, 
-                                                       t, k_steps_args)
+        = vizScenBase.get_array_turn_df_for_t_scenBase(
+            tuple_paths, t, k_steps_args, 
+            algos_for_not_learning=["DETERMINIST","RD-DETERMINIST",
+                                    "BEST-BRUTE-FORCE","BAD-BRUTE-FORCE", 
+                                    "MIDDLE-BRUTE-FORCE"])
     ## -- plot figures    
     name_dir = os.path.join("tests", name_simu)
     vizScenBase.group_plot_on_panel(df_arr_M_T_Ks, df_ben_cst_M_T_K, t, name_dir, 
