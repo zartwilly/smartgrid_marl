c5976c3277894c2313995fb72699f40c606ccab4
EHOUNOU Joseph-Wilfried <jwehounou@cesi.fr>
2021-01-26 15:22:47 +0100

    automate: ajout de la fonction get_or_create.

--- fonctions_auxiliaires.py
+++ fonctions_auxiliaires.py
@@ -833,6 +833,100 @@ def generate_Pi_Ci_Si_Simax_by_automate(set1_m_players, set2_m_players,
     
     return arr_pl_M_T_vars
     
+def get_or_create_instance(set1_m_players, set2_m_players, 
+                            t_periods, 
+                            set1_states, 
+                            set2_states,
+                            set1_stateId0_m_players,
+                            set2_stateId0_m_players, 
+                            path_to_arr_pl_M_T, used_instances):
+    """
+    get instance if it exists else create instance.
+
+    set1 = {state1, state2} : set of players' states 
+    set2 = {state2, state3}
+    
+    Parameters
+    ----------
+    set1_m_players : integer
+        DESCRIPTION.
+        Number of players having their states belonging to set1.
+    set2_m_players : integer
+        DESCRIPTION.
+        Number of players having their states belonging to set2.
+    t_periods : integer
+        DESCRIPTION.
+        Number of periods in the game
+    set1_states : set
+        DESCRIPTION.
+        set of states creating the group set1
+    set2_states: set
+        DESCRIPTION.
+        set of states creating the group set1
+    set1_stateId0_m_players : Integer
+        DESCRIPTION. 
+        Number of players in the set1 having the state equal to the first state in set1
+    set2_stateId0_m_players : Integer
+        DESCRIPTION. 
+        Number of players in the set2 having the state equal to the first state in set2
+    path_to_arr_pl_M_T : string
+        DESCRIPTION.
+        path to save/get array arr_pl_M_T
+        example: tests/AUTOMATE_INSTANCES_GAMES/\
+                    arr_pl_M_T_players_set1_{m_players_set1}_set2_{m_players_set2}\
+                        _periods_{t_periods}.npy
+    used_instances : boolean
+        DESCRIPTION.
+
+    Returns
+    -------
+    arr_pl_M_T_vars : array of 
+        DESCRIPTION.
+
+    """
+    arr_pl_M_T_vars = None
+    filename_arr_pl = AUTOMATE_FILENAME_ARR_PLAYERS_ROOT.format(
+                        set1_m_players, set2_m_players, set1_stateId0_m_players,
+                        set2_stateId0_m_players, t_periods)
+    # path_to_save = os.path.join(*["tests", "AUTOMATE_INSTANCES_GAMES"])
+    
+    if os.path.exists(path_to_arr_pl_M_T):
+        # read arr_pl_M_T
+        if used_instances:
+            arr_pl_M_T_vars \
+                = np.load(os.path.join(*[path_to_arr_pl_M_T,filename_arr_pl]),
+                          allow_pickle=True)
+            print("READ INSTANCE GENERATED")
+            
+        else:
+            # create arr_pl_M_T when used_instances = False
+            arr_pl_M_T_vars \
+                = generate_Pi_Ci_Si_Simax_by_automate(
+                    set1_m_players, set2_m_players, 
+                    t_periods, 
+                    set1_states, 
+                    set2_states,
+                    set1_stateId0_m_players,
+                    set2_stateId0_m_players)
+            
+            save_instances_games(arr_pl_M_T_vars, filename_arr_pl, 
+                                 path_to_save=path_to_arr_pl_M_T)
+            print("CREATE INSTANCE used_instance={}".format(used_instances))
+    else:
+        # create arr_pl_M_T
+        arr_pl_M_T_vars \
+                = generate_Pi_Ci_Si_Simax_by_automate(
+                    set1_m_players, set2_m_players, 
+                    t_periods, 
+                    set1_states, 
+                    set2_states,
+                    set1_stateId0_m_players,
+                    set2_stateId0_m_players)
+        save_instances_games(arr_pl_M_T_vars, filename_arr_pl, 
+                             path_to_save=path_to_arr_pl_M_T)
+        print("NO PREVIOUS INSTANCE GENERATED: CREATE NOW !!!")
+            
+    return arr_pl_M_T_vars    
     
 ###############################################################################
 #            generate Pi, Ci, Si by automate --> fin
