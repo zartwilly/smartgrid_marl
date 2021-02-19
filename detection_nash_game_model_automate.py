1fa2244b56717ca0e76c390b0fa13fae4e673ea9
EHOUNOU Joseph-Wilfried <jwehounou@cesi.fr>
2021-02-19 10:37:43 +0100

    AUTOMATE: NASH algo mettre le prix pi_sg_{plus,minus} a 0 quand le production ou la consommation est nulle.

--- detection_nash_game_model_automate.py
+++ detection_nash_game_model_automate.py
@@ -1333,6 +1333,10 @@ def nash_balanced_player_game_perf_t_USE_DICT_MODE_PROFIL(
                                 }
                     
                 # pi_sg_{plus,minus} of shape (T_PERIODS,)
+                if np.isnan(pi_sg_plus_t):
+                    pi_sg_plus_t = 0
+                if np.isnan(pi_sg_minus_t):
+                    pi_sg_minus_t = 0
                 pi_sg_plus_T_algo[t] = pi_sg_plus_t
                 pi_sg_minus_T_algo[t] = pi_sg_minus_t
                 pi_0_plus_T_algo[t] = pi_0_plus_t
